1dc2ac5333a926f727f288db21d172f415ce7d1b
LiuYH <294429341@qq.com>
2019-01-17 14:03:49 +0800

    Add files via upload
    添加log

--- app/src/main/java/com/uberspot/a2048/helper/TTEventLog.java
+++ app/src/main/java/com/uberspot/a2048/helper/TTEventLog.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 
 import com.crashlytics.android.answers.Answers;
 import com.crashlytics.android.answers.CustomEvent;
+import com.google.android.exoplayer2.util.ParsableNalUnitBitArray;
 
 /**
  * Created by liu on 2018/12/17.
@@ -43,6 +44,26 @@ public class TTEventLog {
             .putCustomAttribute(key, value));
         } catch (Exception e) {
             TLog.logE(e);
+        }
+    }
+
+    public void LogException(Exception ex) {
+
+    }
+
+    public void LogAdSuccess(String adtag) {
+        try {
+            LogEvent("AD_"+adtag + "_Success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void LogAdFailed(String adtag, String errorMsg) {
+        try {
+            LogEvent("AD_"+adtag + "_Failed", "failed", errorMsg);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
